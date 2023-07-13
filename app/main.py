e970ab5d516e3abc1cbdceb9d29fc7a5e1c31fa4
Suyeb Ansari <suyebansari786@gmail.com>
2023-07-13 14:58:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask"
+# @app.route("/")
+# def hello():
+#     return "Hello World from Flask"
 
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+# if __name__ == "__main__":
+#     # Only for debugging while developing
+#     app.run(host='0.0.0.0', debug=True, port=80)
+
+print("Hello World")
